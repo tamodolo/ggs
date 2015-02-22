90d6d00ede70127c2b9fd7843ba46b88be6eae94
id <tamodolo@gmail.com>
2015-02-21 23:47:07 -0200

    translated to english
    this is cosmetic. just to reach more people.

--- ggs.py
+++ ggs.py
@@ -15,27 +15,27 @@ class main(QMainWindow):
 
         QToolTip.setFont(QFont('SansSerif', 10))
 
-        btn = QPushButton('Abrir Lista de Jogos', self)
-        btn.setToolTip('Abra sua lista de jogos aqui')
+        btn = QPushButton('Open File', self)
+        btn.setToolTip('Open your game list here')
         btn.resize(btn.sizeHint())
         btn.move(50,50)
-        btn.clicked.connect(self.abrir)
+        btn.clicked.connect(self.openfile)
 
-        self.lbl1 = QLabel('Seu próximo jogo vai aparecer aqui',self)
+        self.lbl1 = QLabel('Your next game will be showed here',self)
         self.lbl1.resize(250,20)
         self.lbl1.move(200,50)
 
         self.setGeometry(300, 300, 500, 200)
-        self.setWindowTitle('Escolhe Jogo')    
+        self.setWindowTitle('Generic Game Selector')    
         self.show()
 
 
-    def abrir(self):
-        fname = QFileDialog.getOpenFileName(self, 'Abrir Lista', '.')
+    def openfile(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open List', '.')
         f = open(fname[0])
         data = f.read().split("\n")
         pick = random.choice(data)
-        self.lbl1.setText('Comece a jogar ' + str(pick))
+        self.lbl1.setText('Start to play ' + str(pick))
         
             
 
